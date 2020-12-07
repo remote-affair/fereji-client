1c456f879206e031cef50674964134af26ad4afe
namatitiben <namatitiben@gmail.com>
2020-12-07 20:51:11 +0300

    Intial authentication component setup

---
+++ src/app/modules/user-auth/user-auth.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { UserAuthRoutingModule } from './user-auth-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    UserAuthRoutingModule
+  ]
+})
+export class UserAuthModule { }
