50a28e5a13d9230e9030e2f7367b43e0b06feb23
Henry Ohanga <ohanga.henry@gmail.com>
2020-12-11 03:16:57 +0300

    add canActivate auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./modules/user-auth/user-auth.module').then(
+        module => module.UserAuthModule,
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/modules/user-auth/sign-up/sign-up.module.ts
+++ src/app/modules/user-auth/sign-up/sign-up.module.ts
@@ -5,5 +5,6 @@ import { SignUpComponent } from './sign-up.component';
 @NgModule({
   declarations: [SignUpComponent],
   imports: [CommonModule],
+  exports: [SignUpComponent],
 })
 export class SignUpModule {}
