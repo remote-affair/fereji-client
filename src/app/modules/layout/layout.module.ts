6345cce130581add99a5c7a89ce627abdfbfb4d0
Henry Ohanga <ohanga.henry@gmail.com>
2020-12-12 17:59:45 +0300

    update the dashboard layout

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -6,7 +6,12 @@ import { FooterComponent } from './components/footer/footer.component';
 import { GuestLayoutComponent } from './components/guest-layout/guest-layout.component';
 import { DashboardLayoutComponent } from './components/dashboard-layout/dashboard-layout.component';
 import { RouterModule } from '@angular/router';
-import { ClrIconModule } from '@clr/angular';
+import {
+  ClrIconModule,
+  ClrPopoverModule,
+  ClrVerticalNavModule,
+} from '@clr/angular';
+import { AsideComponent } from './components/aside/aside.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +19,14 @@ import { ClrIconModule } from '@clr/angular';
     FooterComponent,
     GuestLayoutComponent,
     DashboardLayoutComponent,
+    AsideComponent,
   ],
-  imports: [CommonModule, RouterModule, RouterModule, ClrIconModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    RouterModule,
+    ClrIconModule,
+    ClrVerticalNavModule,
+  ],
 })
 export class LayoutModule {}
