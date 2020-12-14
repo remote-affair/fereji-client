9bd2abf37b20dc8707eddef8830217e5e7e62c38
Henry Ohanga <ohanga.henry@gmail.com>
2020-12-15 01:14:51 +0300

    deactivate auth route on dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,7 @@ const routes: Routes = [
   {
     path: 'dashboard',
     component: DashboardLayoutComponent,
-    canActivate: [AuthGuard],
+    // canActivate: [AuthGuard],
     loadChildren: () =>
       import('./modules/dashboard/dashboard.module').then(
         module => module.DashboardModule,
