6a172b159dbdcfb574f867247d68a7d14084f9e6
namatitiben <namatitiben@gmail.com>
2020-12-16 13:35:30 +0300

    Login form design

--- src/app/modules/user-auth/login/login.component.spec.ts
+++ src/app/modules/user-auth/login/login.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
+import { TopOffsetTitleModule } from '@fereji/modules/layout/components/top-offset-title/top-offset-title.module';
 
 import { LoginComponent } from './login.component';
 
@@ -11,7 +12,7 @@ describe('LoginComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      imports: [ReactiveFormsModule, RouterTestingModule],
+      imports: [ReactiveFormsModule, RouterTestingModule, TopOffsetTitleModule],
     }).compileComponents();
   });
 
