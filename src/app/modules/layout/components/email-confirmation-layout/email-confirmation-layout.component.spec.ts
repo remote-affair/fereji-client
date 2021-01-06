44962217a9024619e1f9f519084859675a373a6a
Henry Ohanga <ohanga.henry@gmail.com>
2021-01-06 10:25:40 +0300

    add serve prod command

--- src/app/modules/layout/components/email-confirmation-layout/email-confirmation-layout.component.spec.ts
+++ src/app/modules/layout/components/email-confirmation-layout/email-confirmation-layout.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { LayoutModule } from '../../layout.module';
 
 import { EmailConfirmationLayoutComponent } from './email-confirmation-layout.component';
 
@@ -9,6 +11,7 @@ describe('EmailConfirmationLayoutComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [EmailConfirmationLayoutComponent],
+      imports: [RouterTestingModule, LayoutModule],
     }).compileComponents();
   });
 
